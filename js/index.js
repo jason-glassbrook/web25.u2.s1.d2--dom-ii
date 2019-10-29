ac1c87e4dfbba0dd4908a0f054b66235d2c31d96
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-29 17:23:27 -0400

    add mousedown/up events for buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+"use strict";
+
+/***********************************************************
+  event functions
+***********************************************************/
+
+/// mousedown + mouseup ///
+const buttonClickClass = "click";
+
+const buttonMouseDown = function (ev) {
+  if (ev.button === 0) { // left button pressed
+    ev.target.classList.add (buttonClickClass);
+  }
+  ev.stopPropagation ();
+};
+
+const buttonMouseUp = function (ev) {
+  if (ev.button === 0) { // left button pressed
+    ev.target.classList.remove (buttonClickClass);
+  }
+  ev.stopPropagation ();
+};
+
+/***********************************************************
+  add events
+***********************************************************/
+
+/// mousedown + mouseup ///
+const buttons = document.querySelectorAll (".btn");
+buttons.forEach (
+  (el) => {
+    el.addEventListener ("mousedown" , buttonMouseDown);
+    el.addEventListener ("mouseup" , buttonMouseUp);
+  }
+);
