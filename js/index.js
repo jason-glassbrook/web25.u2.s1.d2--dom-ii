d55eb41ffb8ab91a532375ff36c4d1172963ac89
o1123516766542617 <o1123516766542617@outlook.com>
2019-10-29 19:00:43 -0400

    add math functions, color functions, heading mouseover functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,83 @@
 "use strict";
 
 /***********************************************************
+  helper functions
+***********************************************************/
+
+/***************************************
+  Math
+***************************************/
+
+/// "clamp" number between lower and upper bounds ///
+Math.clamp = function (x , lower , upper) {
+  return (
+    Math.min (Math.max (x , upper) , lower)
+  );
+}
+
+/// random number between lower and upper bounds ///
+Math.randomNumWithin = function (lower , upper) {
+  return (
+    lower + ((Math.random ()) * (upper - lower))
+  );
+}
+
+/// random integer between lower and upper bounds ///
+Math.randomIntWithin = function (lower , upper) {
+  lower = Math.floor (lower);
+  upper = Math.ceil (upper);
+  return (
+    lower + (Math.floor ((Math.random ()) * (upper - lower)))
+  );
+}
+
+/***************************************
+  RGB colors
+***************************************/
+
+const rgb = {
+  fromHex: (()=>{}),
+  mod:     (()=>{}),
+  clamp:   (()=>{}),
+  toCss:   (()=>{})
+};
+
+/// convert hex to rgb channels ///
+rgb.fromHex = function (hexColor) { // doesn't protect against keyword colors
+  const r = parseInt(hexColor.substr(1,2), 16);
+  const g = parseInt(hexColor.substr(3,2), 16);
+  const b = parseInt(hexColor.substr(5,2), 16);
+  return (
+    [r , g , b]
+  );
+}
+
+/// convert rgb channels to css function string
+rgb.toCss = function (cs) {
+  return (
+    `rgb(${cs[0]} , ${cs[1]} , ${cs[2]})`
+  );
+}
+
+/// mod ///
+rgb.mod = function (cs) {
+  return (
+    cs.map(
+      (c) => (c % 256)
+    )
+  );
+};
+
+/// clamp ///
+rgb.clamp = function (cs) {
+  return (
+    cs.map(
+      (c) => (Math.clamp(c , 0 , 255))
+    )
+  );
+};
+
+/***********************************************************
   event functions
 ***********************************************************/
 
@@ -34,6 +111,35 @@ const imageMouseOut = function (ev) {
   ev.stopPropagation ();
 };
 
+/// mouseover ///
+const headingHoverClass = "heading-hover";
+
+// this returns another function that has a unique closure state
+const headingMouseOver = function (el) {
+  // get original color to modify in closure
+  let color = rgb.fromHex (el.style.color);
+  console.log (color);
+  // define color changing function
+  const changeElementColor = function (ev) {
+    // generate random rgb offset
+    const offset = color.map (
+      () => (Math.randomIntWithin (-5 , +5))
+    );
+    // add offset and clamp
+    color = rgb.clamp (
+      color.map (
+        (c , i) => (c + offset[i])
+      )
+    );
+    // set color of element
+    el.style.color = rgb.toCss (color);
+    // stop bubbles
+    ev.stopPropagation ();
+  };
+  // return function
+  return (changeElementColor);
+};
+
 /***********************************************************
   add events
 ***********************************************************/
@@ -55,3 +161,11 @@ images.forEach (
     el.addEventListener ("mouseleave" , imageMouseOut);
   }
 );
+
+/// mouseover ///
+const headings = document.querySelectorAll ("h1 , h2 , h3 , h4 , h5 , h6");
+headings.forEach (
+  (el) => {
+    el.addEventListener ("mouseover" , headingMouseOver (el));
+  }
+)
